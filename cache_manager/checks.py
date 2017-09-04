e6dd21c64a32c45b779e87f49d6658c765653b22
Константинов Александр Сергеевич <konstantinov@rosenergo.com>
2017-09-04 22:35:22 +0300

    autocommit

--- cache_manager/checks.py
+++ cache_manager/checks.py
@@ -26,7 +26,7 @@ class GitBlobWrapper(object):
         LOG.warning(self.operation)
         self.check, self.ext = os.path.splitext(self.filename)
         self.ext = self.ext.strip('.').lower()
-        LOG.error(self.check, '->>', self.extension)
+        LOG.error(self.check, '->>', self.ext)
         self.full_path = os.path.join(path, self.operation, self.filename)
         LOG.warning(self.full_path)
 
