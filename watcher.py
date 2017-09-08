3fbf1c6bbf9f50506aade3436148cbc7a855bbf4
Константинов Александр Сергеевич <konstantinov@rosenergo.com>
2017-09-08 10:15:42 +0300

    autocommit

--- watcher.py
+++ watcher.py
@@ -1,32 +1,37 @@
+import logging
+import os.path
+import platform
 import sys
 import time
-import logging
 from subprocess import Popen # , PIPE
+
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 # prc = None
 
+PROC_NAME = 'python{}'.format('3' if 'linux' in platform.system().lower() else '')
+
 class Restarter(PatternMatchingEventHandler):
-    pyt = None
+    pyt = Popen([PROC_NAME, 'run_server.py'])
     def on_modified(self, event):
         super(Restarter, self).on_modified(event)
         what = 'directory' if event.is_directory else 'file'
         logging.info("Modified %s: %s", what, event.src_path)
-        if self.pyt:
-            self.pyt.terminate()
-            logging.info("Python terminated")
-        self.pyt = Popen(['python3', 'run_server.py'])
+        self.pyt.terminate()
+        logging.info("Python terminated")
+        self.pyt = Popen([PROC_NAME, 'run_server.py'])
 
 
 if __name__ == "__main__":
+
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = Restarter(patterns=['*.py'])
+    root = '.'
+    event_handler = Restarter(patterns=['*.py'], ignore_patterns=[os.path.join(root, 'reg', '*')])
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, root, recursive=True)
     observer.start()
     try:
         while True:
