d6aa1ce389833a8073b9ca5c0097df82d2477e4c
Константинов Александр Сергеевич <konstantinov@rosenergo.com>
2017-09-08 11:52:04 +0300

    autocommit

--- server.py
+++ server.py
@@ -129,7 +129,7 @@ async def cached_get_last_checks(request):
     mem_cache.setdefault(key, {'query': query, 'hash': response_hash})
 
     while response_hash == mem_cache[key]['hash']:
-        # print(request.headers)
+        print(request, query)
         mem_cache.refresh_item(key)
         await aio.sleep(0.1)
 
