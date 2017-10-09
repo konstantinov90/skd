ce8f014c6a8fdfe405bf6b475d20b64b5e7b1cbb
konstantinov <konstantnov@rosenergo.com>
2017-10-09 15:58:02 +0300

    autocommit

--- skd.py
+++ skd.py
@@ -29,7 +29,7 @@ async def register_task(_task):
 
 def run_check(extension, check, task):
     imp.reload(aio)
-    imp.reload(utils.db_client)
+    imp.reload(db_client)
     imp.reload(environment)
     aio.run(attrgetter(extension)(environment), check, task)
 
