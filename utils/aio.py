88f90e08d21d831eef80e8e26d1a476dd0e1c820
konstantinov <konstantnov@rosenergo.com>
2017-10-12 17:44:16 +0300

    autocommit

--- utils/aio.py
+++ utils/aio.py
@@ -31,6 +31,4 @@ def run(coroutine, *args):
 
 def loop_run(coroutine, *args):
     loop = aio.new_event_loop()
-    res = loop.run_until_complete(coroutine(*args))
-    print('result', res)
-    return res
+    return loop.run_until_complete(coroutine(*args))
